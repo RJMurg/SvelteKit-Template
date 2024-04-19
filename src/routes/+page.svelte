30978cab69b690a2545893f0bb0942fffb6cfff6
Ruán Murgatroyd <ruan@murgatroyd.me>
2024-04-19 12:45:57 +0100

    more styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,37 @@
 </svelte:head>
 
 <div>
-	<h1 class="title">
-		{data.hello}
+	<h1>
+		Data from serverside component: {data.hello}
 	</h1>
+
+	<hr />
+
+	<h1 class="title">I am a title!</h1>
+
+	<h2 class="subtitle">I am a subtitle!</h2>
+
+	<p class="xlarge">X-Large!</p>
+
+	<p class="large">Large!</p>
+
+	<p class="medium">Medium!</p>
+
+	<p>Normal Text</p>
+
+	<p class="small">Small!</p>
+
+	<p class="xsmall">X-Small!</p>
+
+	<hr />
+
+	<p class="left">Holy Left!</p>
+
+	<p class="right">On your Right!</p>
+
+	<p class="italic">Falling over</p>
+
+	<p class="bold">Very thicc</p>
+
+	<p class="bold italic">Doing both!</p>
 </div>
