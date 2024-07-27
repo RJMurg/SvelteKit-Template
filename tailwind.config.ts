77d8b9f1fb798ad4a8d0a04717351aedc56a6e75
Ruán <ruan.murgatroyd@sap.com>
2024-07-27 15:09:35 +0100

    Started adding themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,23 @@
 import type { Config } from 'tailwindcss';
 
 export default {
+	presets: [
+		require('@rjmurg/legskit/tailwind'),
+	],
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
 	theme: {
-		extend: {}
+	 extend: {}
 	},
-
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
 	daisyui: {
-		themes: ["dark"],
-	},
-
-	plugins: [
-		require('@tailwindcss/typography'),
-		require('daisyui')
-	]
+	 themes: [
+	  'light',
+	  'dark',
+	  'bumblebee',
+	  'emerald',
+	  'corporate',
+	  'retro',
+	  'cyberpunk',
+	 ]
+	}
 } as Config;
